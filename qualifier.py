aae01639d51c94acec55abbf17408442b9c0f78f
Johannes Christ <jc@jchri.st>
2019-01-22 20:15:29 +0100

    Add updated code.

--- qualifier.py
+++ qualifier.py
@@ -1,5 +1,5 @@
 """
-A GUI application which shows you a simple login form.
+A GUI application for a rocketship launch control panel.
 This application is built on tkinter. See the official
 documentation and linked resources here:
     https://docs.python.org/3/library/tkinter.html
@@ -7,31 +7,56 @@ documentation and linked resources here:
 Requirements:
     Python 3.
 """
-
 import tkinter as tk
 
 
-class LoginApplication(tk.Frame):
+class RocketShipControlPanel(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
-            
+
+        self.pilot = None
+        self.pilot_label = None
+        self.launch_button = Null
+
     def create_form(self):
-        # Create the login button along with username
-        # and password fields.
-        self.login_button = tk.Button(self)
-        self.login_button['text'] = "Login"
-        self.login_button['command'] = self.do_login
-        self.login_button.pack(side="bottom")
+        """
+        Create the input fields, labels,
+        and buttons when called.
+        """
 
-    def do_login(self):
-        # When the user clicks the login button, this callback
-        # is invoked. Change it to have it update the login button
-        # text to something completely meaningless.
-        raise NotImplementedError("Don't know how to login.")
+        self.pilot_label = tkinter.Label(
+            self,
+            text="Pilot: ",
+        )
+        self.pilot = tk.Entry(
+            self,
+            width=30,
+        )
+        self.pilot_label.pack(side=TOP)
+        self.pilot.pack(side=tk.TOP)
 
+        self.launch_button = tk.Button(
+            self,
+            text="Launch",
+            command=self.do_countdown,
+        )
+        self.launch_button.pack(side=tk.BOTTOM)
+
+    def do_countdown(self):
+        """
+        When the user clicks the login button, this callback
+        is invoked. Make it do a countdown. The first time
+        it is clicked, the button text should change to "3".
+        The next time to "2", then to "1", and then to "LIFTOFF!".
+
+        If the username or the password are blank, this
+        callback should not do anything.
+        """
+
+        raise NotImplementedError
 
 root = tk.Tk()
-app = LoginApplication(master=root)
+app = RocketShipControlPanel(master=root)
 app.mainloop()
